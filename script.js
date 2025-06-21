0037776565659937ec7ac8f246a9dc2acbd03e29
mrlong37 <37mrlong@gmail.com>
2025-06-21 04:34:38 -0500

    worked on javascript; need to import images and stylize

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // Declare variables below to save the different divs of your story.
-let 
-
+let b1 = document.querySelector(".op1");
+let b2 = document.querySelector(".op2");
+let b3 = document.querySelector(".op1aa");
+let b4 = document.querySelector(".op1ab");
+let b5 = document.querySelector(".op1aaaa");
+let b6 = document.querySelector(".op1aaab");
+let b7 = document.querySelector(".op1ba");
+let b8 = document.querySelector(".op1bb");
+let b9 = document.querySelector(".op2a");
+let b10 = document.querySelector(".op2b");
 
 
 
@@ -12,15 +20,46 @@ let
 //  - Then fill in the blanks with the correct variables.
 
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+  b1.addEventListener('click', function(){
+  document.querySelector(".op1a").style.display = "block";
+  document.querySelector(".button1a").style.display = "block";
+ });
 
-// });
+  b2.addEventListener('click', function(){
+  document.querySelector(".op2s").style.display = "block";
+  document.querySelector(".buttons2s").style.display = "block";
+ });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+  b3.addEventListener('click', function(){
+  document.querySelector(".op1aaa").style.display = "block";
+  document.querySelector(".buttons1aaa").style.display = "block";
+ });
 
-// });
+  b4.addEventListener('click', function(){
+  document.querySelector(".op1abs").style.display = "block";
+  document.querySelector(".buttons1bs").style.display = "block";
+ });
 
+  b5.addEventListener('click', function(){
+  document.querySelector(".op1aaaaf").style.display = "block";
+ });
 
-// INSERT_VARIABLE.addEventListener('click', function(){
+  b6.addEventListener('click', function(){
+  document.querySelector(".op1aaabf").style.display = "block";
+ });
 
-// });
+  b7.addEventListener('click', function(){
+  document.querySelector(".op1bas").style.display = "block";
+ });
+
+  b8.addEventListener('click', function(){
+  document.querySelector(".op1bbs").style.display = "block";
+ });
+
+  b9.addEventListener('click', function(){
+  document.querySelector(".op2as").style.display = "block";
+ });
+
+  b10.addEventListener('click', function(){
+  document.querySelector(".op2bs").style.display = "block";
+ });
